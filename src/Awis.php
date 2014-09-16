0e4d5baada368a5661fe3875380d4238ceed7588
Nenad Tičarić <nticaric@gmail.com>
2014-09-16 22:20:44 +0200

    awis class

---
+++ src/Awis.php
@@ -0,0 +1,14 @@
+<?php
+
+class Awis {
+    
+    protected $accessKeyId;
+    protected $secretAccessKey;
+
+    public function __construct($accessKeyId, $secretAccessKey)
+    {
+        $this->accessKeyId     = $accessKeyId;
+        $this->secretAccessKey = $secretAccessKey;
+    }
+    
+}

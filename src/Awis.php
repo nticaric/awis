2883a6ffc31022f94391d1b7d4fa757f28ed0f59
Nenad Tičarić <nticaric@gmail.com>
2014-09-18 14:46:42 +0200

    getCategoryListings

--- src/Awis.php
+++ src/Awis.php
@@ -80,9 +80,31 @@ class Awis {
         return $client->send($request);
     }
 
-    public function getCategoryListings()
+    public function getCategoryListings($url, $path, $sortBy, $recursive, $start, $count, $descriptions = 'TRUE')
     {
+        if( $count > 20) $count = 20;
 
+        $this->dt = Carbon::now();
+
+        $client = new Client();
+        $request = $client->createRequest('GET', $this->endPoint);
+        $query = $request->getQuery();
+        $query->set('Action', 'CategoryListings');
+        $query->set('AWSAccessKeyId', $this->accessKeyId);
+        $query->set('SignatureMethod', $this->signatureMethod);
+        $query->set('SignatureVersion', $this->signatureVersion);
+        $query->set('Timestamp', $this->dt->toISO8601String());
+        $query->set('Url', $url);
+        $query->set('ResponseGroup', 'Listings');
+        $query->set('Path', $path);
+        $query->set('SortBy', $sortBy);
+        $query->set('Recursive', $recursive);
+        $query->set('Start', $start);
+        $query->set('Count', $count);
+        $query->set('Descriptions', $descriptions);
+        $query->set('Signature', $this->generateSignature($query));
+
+        return $client->send($request);
     }
 
     public function getSitesLinkingIn()
